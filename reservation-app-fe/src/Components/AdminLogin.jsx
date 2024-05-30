bfb78ab78caec7a197ed7b0c8308d485dc3c8053
krdurgesh610 <kr.durgesh610@gmail.com>
2024-05-30 17:18:28 +0530

    Connect to DataBase & Save the Data

--- reservation-app-fe/src/Components/AdminLogin.jsx
+++ reservation-app-fe/src/Components/AdminLogin.jsx
@@ -2,25 +2,37 @@ import Button from 'react-bootstrap/Button';
 import React, { useState } from 'react'
 import '../Styles/AdminLogin.css'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 function AdminLogin() {
-let [username,setUsername] = useState("")
+let [email,setEmail] = useState("")
 let [password,setPassword] = useState("")
 
-function verify(){
-  if(username=="abcd" && password==1234){
-    alert("Login Succefull")
-  }
-  else{
-    alert("Login failed")
-  }
+function verify(e){
+  e.preventDefault()
+  axios.post(`http://localhost:8080/api/admins/verify-by-email?email=${email}&password=${password}`)
+  .then((res)=>{
+    alert("Verify Succesfull")
+  })
+  .catch((err)=>{
+    alert("Verify Failed")
+  })
 }
+
+// function verify(){
+//   if(email=="abcd" && password==1234){
+//     alert("Login Succefull")
+//   }
+//   else{
+//     alert("Login failed")
+//   }
+// }
 
   return (
     <div className='adminLogin'>
         <form onSubmit={verify} action="">
             <label htmlFor="">User Name</label>
-            <input type="text" value={username} onChange={(e)=>{setUsername(e.target.value)}} placeholder='Enter User Name'/>
+            <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='Enter User Name'/>
 
             <label htmlFor="">Password</label>
             <input type="text" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='Enter Password'/>
--- reservation-app-fe/src/Components/AdminSignUp.jsx
+++ reservation-app-fe/src/Components/AdminSignUp.jsx
@@ -1,34 +1,52 @@
 import React, { useState } from 'react'
 import '../Styles/AdminSignUp.css'
+import axios from 'axios'
 function AdminSignUp() {
 
     let [name,setName] = useState("")
     let [email,setEmail] = useState("")
     let [phone,setPhone] = useState("")
-    let [gstno,setGstno] = useState("")
-    let [travels,setTravels] = useState("")
+    let [gst_number,setGst_number] = useState("")
+    let [travels_name,setTravels_name] = useState("")
     let [password,setPassword] = useState("")
+
+    let data={
+      name,email,phone,gst_number,travels_name,password
+    }
+
+    function createAdmin(e){
+      e.preventDefault()
+      axios.post('http://localhost:8080/api/admins',data)
+      .then((res)=>{
+        alert("data Added Succesfully")
+        console.log(res);
+      })
+      .catch((err)=>{
+        alert("Invalid Data")
+        console.log(err);
+      })
+    }
 
   return (
     <div className='adminSignUp'>
-      <form action="">
+      <form onSubmit={createAdmin} action="">
         <label htmlFor="">Name</label>
         <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} required placeholder='Enter the Name'/>
 
         <label htmlFor="">Email</label>
-        <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} required placeholder='Enter the Email Id'/>
+        <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} required placeholder='Enter the Email Id'/>
 
         <label htmlFor="">Phone</label>
-        <input type="text" value={phone} onChange={(e)=>{setPhone(e.target.value)}} required placeholder='Enter the Phone number'/>
+        <input type="tel"  value={phone} onChange={(e)=>{setPhone(e.target.value)}} required placeholder='Enter the Phone number'/>
 
         <label htmlFor="">GST Number</label>
-        <input type="text" value={gstno} onChange={(e)=>{setGstno(e.target.value)}} required placeholder='Enter the GST NUmber'/>
+        <input type="text" value={gst_number} onChange={(e)=>{setGst_number(e.target.value)}} required placeholder='Enter the GST NUmber'/>
 
         <label htmlFor="">Travels Name</label>
-        <input type="text" value={travels} onChange={(e)=>{setTravels(e.target.value)}} required placeholder='Enter the Travels Name'/>
+        <input type="text" value={travels_name} onChange={(e)=>{setTravels_name(e.target.value)}} required placeholder='Enter the Travels Name'/>
 
         <label htmlFor="">Password</label>
-        <input type="text" value={password} onChange={(e)=>{setPassword(e.target.value)}} required placeholder='Enter the Password'/>
+        <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} required placeholder='Enter the Password'/>
 
         <button>SignUp</button>
       </form>
